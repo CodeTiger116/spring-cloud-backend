d55496b6b3316f238eed453303b5d1196e4cd830
hanhu <13253347105@163.com>
2023-04-09 21:05:25 +0800

    消费者订单模块

---
+++ cloud-consumer-order80/src/main/java/com/hanhu/sprngcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package com.hanhu.sprngcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hanhu
+ * @date 2023/4/9 20:13
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/hanhu/sprngcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,18 @@
+package com.hanhu.sprngcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author hanhu
+ * @date 2023/4/9 20:24
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
