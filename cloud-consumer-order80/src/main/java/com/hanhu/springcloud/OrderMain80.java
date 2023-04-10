68a28b3f091d91bf80c76561a3a3e926bd1a62dd
hanhu <13253347105@163.com>
2023-04-10 15:36:00 +0800

    工程重构

--- cloud-consumer-order80/src/main/java/com/hanhu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/hanhu/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.hanhu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hanhu
  * @date 2023/4/9 20:13
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-eureka-server7001/src/main/java/com/hanhu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/hanhu/springcloud/EurekaMain7001.java
@@ -2,16 +2,12 @@ package com.hanhu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author  hanhu
- * @date  2023/4/9 17:27
- */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
