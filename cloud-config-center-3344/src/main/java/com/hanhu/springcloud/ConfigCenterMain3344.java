2a904d454bb0db25ab98fa8d45ece8c804af7bea
hanhu <13253347105@163.com>
2023-05-05 22:21:19 +0800

    SpringCloud Config分布式配置中心

---
+++ cloud-config-center-3344/src/main/java/com/hanhu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.hanhu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hanhu
+ * @date 2023/5/4 17:10
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
